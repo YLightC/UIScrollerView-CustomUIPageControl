6bb064237745863389cfd2be74f35adf43b21fc5
yaosixu <jason_yaosixu@163.com>
2016-12-09 12:22:51 +0800

    添加自定义cell左滑操作

---
+++ Banner/CustomSwipeTableViewCell.swift
@@ -0,0 +1,201 @@
+//
+//  CustomSwipeTableViewCell.swift
+//  Banner
+//
+//  Created by yaosixu on 2016/12/8.
+//  Copyright © 2016年 Jason_Yao. All rights reserved.
+//
+
+import UIKit
+
+///添加子视图必须添加到 myContentView
+class CustomSwipeTableViewCell: UITableViewCell {
+
+    let myContentView = UIView()
+    /// button的默认宽度是100，可以通过buttonWidth设置
+    var buttonArray: [UIButton] = [] {
+        didSet {
+            addButton()
+        }
+    }
+    var buttonActionArray: [((Void) -> Void)?] = []
+    var buttonWidth: CGFloat = 100
+    
+    private var startPoint: CGPoint = CGPoint(x: 0, y: 0)
+    private var lastPoint: CGPoint = CGPoint(x: 0, y: 0)
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.accessoryType = .none
+        self.focusStyle = .custom
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        self.selectionStyle = .none
+        // Configure the view for the selected state
+    }
+    
+    private func addButton() {
+        let width = UIScreen.main.bounds.size.width
+        let height = self.bounds.size.height - 1
+        
+        for i in 0..<buttonArray.count {
+            let buttonItem = buttonArray[i]
+            buttonItem.frame = CGRect(x: width - CGFloat(buttonArray.count - i) * buttonWidth, y: 0, width: buttonWidth, height: height)
+            buttonItem.tag = i
+            self.addSubview(buttonItem)
+            buttonItem.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+        }
+        addContentView()
+    }
+    
+    private func addContentView() {
+        let contentViewSize = CGSize(width: UIScreen.main.bounds.size.width, height: contentView.bounds.size.height)
+        myContentView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: contentViewSize)
+        self.contentView.backgroundColor = UIColor.black
+        myContentView.backgroundColor = UIColor.red
+        self.addSubview(myContentView)
+        
+        let panContentView = UIPanGestureRecognizer(target: self, action: #selector(panContentViewAction))
+        panContentView.delegate = self
+        myContentView.addGestureRecognizer(panContentView)
+    }
+    
+    @objc private func panContentViewAction(panAction: UIPanGestureRecognizer) {
+        let point = panAction.translation(in: self.myContentView)
+        var moveX: CGFloat = 0
+        if panAction.state == .began {
+            startPoint = point
+        } else if panAction.state == .changed {
+            if myContentView.frame.origin.x >= -(buttonWidth * CGFloat(buttonArray.count)) &&  myContentView.frame.origin.x <= 0 {
+                lastPoint.x -= point.x
+                if myContentView.frame.origin.x - lastPoint.x < -(buttonWidth * CGFloat(buttonArray.count)) {
+                    moveX = -(buttonWidth * CGFloat(buttonArray.count))
+                } else {
+                    moveX = myContentView.frame.origin.x - lastPoint.x
+                    if moveX > 0 {
+                        moveX = 0
+                    }
+                }
+                panAnimation(duringTime: 0.1, targetView: myContentView, offSetX: moveX)
+            }
+        } else if panAction.state == .ended {
+            if (lastPoint.x - startPoint.x >= (buttonWidth * CGFloat(buttonArray.count)) / 2) {
+                moveX = 0
+            } else if lastPoint.x - startPoint.x <= -(buttonWidth * CGFloat(buttonArray.count)) / 2 {
+                moveX = -(buttonWidth * CGFloat(buttonArray.count))
+            } else {
+                if startPoint.x > lastPoint.x {
+                    moveX = 0
+                } else if startPoint.x < lastPoint.x {
+                    moveX = -(buttonWidth * CGFloat(buttonArray.count))
+                }
+            }
+            panAnimation(duringTime: 0.2, targetView: myContentView, offSetX: moveX)
+        } else {
+            panAnimation(duringTime: 0.3, targetView: myContentView, offSetX: 0)
+        }
+        lastPoint = point
+    }
+    
+    ///点击自定义按钮后cell恢复原状
+    @objc private func tapButton(button: UIButton) {
+        print("\(#function),button.tag = \(button.tag)")
+        buttonActionArray[button.tag]?()
+        panAnimation(duringTime: 0.3, targetView: myContentView, offSetX: 0)
+    }
+    
+    //点击cell时触发
+    override func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if haveOffsetCell(gestureRecognizer: gestureRecognizer) {
+            return false
+        }
+        return true
+    }
+    
+    //开始滑动时触发
+    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let panAction = gestureRecognizer as? UIPanGestureRecognizer else {
+            return true
+        }
+        
+        let point = panAction.translation(in: myContentView)
+        if fabs(point.y - startPoint.y) > 2 {
+            return false
+        }
+        return true
+    }
+    
+    ///判断当前tableView显示的cell中，有没有发生偏移的
+    private func haveOffsetCell(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let tableView = findSuperViewIsUITableView(gestureRecognizer: gestureRecognizer) else {
+            return false
+        }
+        
+        guard let visalCells = tableView.visibleCells as? [CustomSwipeTableViewCell] else {
+            return false
+        }
+        
+        for visalCellsItem in visalCells {
+            if visalCellsItem.myContentView.frame.origin.x < 0 {
+                return true
+            }
+        }
+        return false
+    }
+    
+    ///找出当前cell所在的UITableView
+    private func findSuperViewIsUITableView(gestureRecognizer: UIGestureRecognizer) -> UITableView? {
+        guard let view = gestureRecognizer.view else {
+            return nil
+        }
+        var cellSuperView: UIView? = view
+        while cellSuperView != nil {
+            if (cellSuperView?.isKind(of: UITableView.self))! {
+                return cellSuperView as? UITableView
+            }
+            cellSuperView = cellSuperView?.superview
+        }
+        return nil
+    }
+    
+}
+
+///cell 滑动动画
+fileprivate func panAnimation(duringTime: TimeInterval, targetView: UIView, offSetX: CGFloat) {
+    DispatchQueue.main.async {
+        UIView.animate(withDuration: duringTime, animations: {
+            targetView.frame.origin.x = offSetX
+        })
+    }
+}
+
+extension UITableView : UIGestureRecognizerDelegate {
+    
+    //如果当前有cell发生了偏移，再次滑动cell的时候应该阻塞cell的滑动动作，以及tableView的滑动动作
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        guard let visalCells = self.visibleCells as? [CustomSwipeTableViewCell] else {
+            return true
+        }
+        
+        var result = true
+        for item in visalCells {
+            if item.myContentView.frame.origin.x < 0 {
+                result = false
+                panAnimation(duringTime: 0.3, targetView: item.myContentView, offSetX: 0)
+            }
+        }
+        
+        return result
+    }
+}
--- Banner/ViewController.swift
+++ Banner/ViewController.swift
@@ -12,18 +12,40 @@ class ViewController: UIViewController {
 
     let imageNameArray: [String] = ["0","1","2"]
     var pageControl: CustomPageControl!
+    let button: UIButton = UIButton()
+    
+    let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let bannerFrame = CGRect(x: 0, y: 100, width: self.view.bounds.size.width, height: 200)
-        let bannerSocrller = BannerScrollView(frame: bannerFrame, startIndex: 0, imageNameArray: imageNameArray)
-        bannerSocrller.bannerScorllerDelegate = self
-        bannerSocrller.time = 2.5
-        bannerSocrller.isAutoScrollerWithTime = true
-        view.addSubview(bannerSocrller)
-        addPageControl()
+        self.automaticallyAdjustsScrollViewInsets = false
+        addTabelView()
+//        let bannerFrame = CGRect(x: 0, y: 100, width: self.view.bounds.size.width, height: 200)
+//        let bannerSocrller = BannerScrollView(frame: bannerFrame, startIndex: 0, imageNameArray: imageNameArray)
+//        bannerSocrller.bannerScorllerDelegate = self
+//        bannerSocrller.time = 2.5
+//        bannerSocrller.isAutoScrollerWithTime = true
+//        view.addSubview(bannerSocrller)
+//        addPageControl()
+//        button.frame = CGRect(x: 100, y: 100, width: 100, height: 30)
+//        button.setTitle("tap Me!", for: .normal)
+//        button.setTitleColor(UIColor.white, for: .normal)
+//        button.backgroundColor = UIColor.black
+//        button.addTarget(self, action: #selector(tapButtonAction), for: .touchUpInside)
+//        view.addSubview(button)
     }
-
+    
+    func tapButtonAction() {
+        guard let url = URL(string: "AboutURLSession://") else {
+            return
+        }
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            print("can not open this Url")
+        }
+    }//JasonYaoSiXu
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -46,3 +68,56 @@ extension ViewController : BannerScrollerViewDelegate {
         pageControl.currentPage = imageIndex
     }
 }
+
+
+extension ViewController {
+    
+    func addTabelView() {
+        tableView.frame = view.bounds
+        tableView.tableFooterView = UIView()
+        tableView.register(CustomSwipeTableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+        
+    }
+    
+}
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath) as? CustomSwipeTableViewCell else {
+            return UITableViewCell()
+        }
+        let button1 = UIButton()
+        button1.setTitle("button1", for: .normal)
+        let button2 = UIButton()
+        button2.setTitle("button2", for: .normal)
+        cell.buttonArray = [button1, button2]
+        cell.buttonActionArray = [nil,nil]
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+//        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
+
+
