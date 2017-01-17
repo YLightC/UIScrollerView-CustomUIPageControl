4cc2c82843876e6b7ca000fd3626782a797e3abf
yaosixu <jason_yaosixu@163.com>
2017-01-17 15:22:31 +0800

    first commit

---
+++ Banner/DateView.swift
@@ -0,0 +1,9 @@
+//
+//  DateView.swift
+//  Banner
+//
+//  Created by yaosixu on 2017/1/17.
+//  Copyright © 2017年 Jason_Yao. All rights reserved.
+//
+
+import Foundation
---
+++ Banner/DateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DateViewController.swift
+//  Banner
+//
+//  Created by yaosixu on 2017/1/16.
+//  Copyright © 2017年 Jason_Yao. All rights reserved.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
